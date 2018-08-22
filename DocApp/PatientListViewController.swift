efb50cd878b81194697d8166396f4d74bcdc61eb
Federico Dazzan <federico@dazzan.net>
2018-08-22 18:46:56 +0200

    Add New Item Functionality

--- DocApp/PatientListViewController.swift
+++ DocApp/PatientListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class PatientListViewController: UITableViewController {
     
     
-    let itemArray = ["Paziente Bot", "Paziente Noob", "Paziente Scrub"]
+    var itemArray = ["Paziente Bot", "Paziente Noob", "Paziente Scrub"]
     
 
     
@@ -51,8 +51,39 @@ class PatientListViewController: UITableViewController {
         
         //code to make cell selection animation "flash"
 //        tableView.deselectRow(at: indexPath, animated: true)
+    
     }
     
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField1 = UITextField()
+        
+        let alert = UIAlertController(title: "Aggiungi Nuovo Paziente", message: "", preferredStyle: .alert)
+        
+        //modified the UiAlertAction by pressing enter on Action and making a closure this way
+        let action = UIAlertAction(title: "Aggiungi Paziente", style: .default) { (action) in
+            //what will happens once the user clicks the Aggiungi nuovo paziente on our UIAlerts
+            self.itemArray.append(textField1.text!) //questa parte va espansa per impedire l'inserimento di stringhe vuote
+            
+            self.tableView.reloadData() //this line repopulates the table data, otherwise it wouldn't be shown
+        
+        }
+        
+        //again after selecting alert.addTextField from xcode, I press enter on the last field and I create a closure
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField1 = alertTextField //I'm doing this because the alertTextField is only avaible to the scope of this closure
+         
+   
+        }
+        
+        alert.addAction(action)
+        
+        //this way I actually show the alert
+        present(alert, animated: true, completion: nil)
+    }
     
     
 
