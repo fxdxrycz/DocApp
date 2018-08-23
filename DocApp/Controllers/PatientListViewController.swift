71b9be9b82270680e44813de97f6cc722874b823
Federico Dazzan <federico@dazzan.net>
2018-08-23 13:38:42 +0200

    Converted to MVC pattern

--- DocApp/Controllers/PatientListViewController.swift
+++ DocApp/Controllers/PatientListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class PatientListViewController: UITableViewController {
     
     
-    var itemArray = ["Paziente Bot", "Paziente Noob", "Paziente Scrub"]
+    var itemArray = [Patient]()
     
     //this is where we store our default settings
     let defaults = UserDefaults.standard
@@ -24,8 +24,23 @@ class PatientListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let newPatient1 = Patient()
+        newPatient1.name = "Manda Noob"
+        itemArray.append(newPatient1)
+        
+        let newPatient2 = Patient()
+        newPatient2.name = "Botty Bot"
+        itemArray.append(newPatient2)
+        
+        let newPatient3 = Patient()
+        newPatient3.name = "Ana McBot"
+        itemArray.append(newPatient3)
+        
+        
+        
+        
         //uso if let per essere sicuro che questo array esista veramente in memoria, questa riga serve a popolare l'array a runtime con l'array salvata nel file .plist su memoria di massa
-        if let items = defaults.array(forKey: "PatientListArray") as? [String] {
+        if let items = defaults.array(forKey: "PatientListArray") as? [Patient] {
             itemArray = items
         }
     }
@@ -45,7 +60,23 @@ class PatientListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "PatientItemCell" , for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].name
+        
+        
+        
+        //chemark code, this can be done with the following ternary operator
+        
+//        if itemArray[indexPath.row].selected == true{
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
+        //ternary operator
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = itemArray[indexPath.row].selected  ? .checkmark : .none 
+        
         
         return cell
     }
@@ -53,6 +84,13 @@ class PatientListViewController: UITableViewController {
     //MARK - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //if i select the bool value is set to the opposite of the current one
+        itemArray[indexPath.row].selected = !itemArray[indexPath.row].selected
+        
+        //necessary to update the view with the checkboxes, they wouldn't show otherwise
+        tableView.reloadData()
+        
         print(indexPath.row)
         
         //code to make cell selection animation "flash"
@@ -73,7 +111,11 @@ class PatientListViewController: UITableViewController {
             //what will happens once the user clicks the Aggiungi nuovo paziente on our UIAlerts
             
             //since I'm in a closure i have to add self everywhere
-            self.itemArray.append(textField1.text!) //questa parte va espansa per impedire l'inserimento di stringhe vuote
+            
+            let newItem = Patient()
+            newItem.name = textField1.text!
+            
+            self.itemArray.append(newItem) //questa parte va espansa per impedire l'inserimento di stringhe vuote
             
             self.defaults.set(self.itemArray, forKey: "PatientListArray")
             
---
+++ DocApp/Data Model/Patient.swift
@@ -0,0 +1,14 @@
+//
+//  Patient.swift
+//  DocApp
+//
+//  Created by Federico Dazzan on 23/08/2018.
+//  Copyright © 2018 Federico Dazzan. All rights reserved.
+//
+
+import Foundation
+
+class Patient {
+    var name: String = ""
+    var selected: Bool = false
+}
